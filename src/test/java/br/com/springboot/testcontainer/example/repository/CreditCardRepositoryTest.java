c081e530816a84c508562e07d4c834f712be8aeb
Felipe Borges <felipeborges@pop-os.localdomain>
2022-09-27 19:55:21 -0300

    Implementa teste de integracao com h2

--- src/test/java/br/com/springboot/testcontainer/example/repository/CreditCardRepositoryTest.java
+++ src/test/java/br/com/springboot/testcontainer/example/repository/CreditCardRepositoryTest.java
@@ -1,8 +1,6 @@
 package br.com.springboot.testcontainer.example.repository;
 
-import br.com.springboot.testcontainer.example.config.PostgresqlDB;
 import br.com.springboot.testcontainer.example.entity.CreditCard;
-import org.junit.ClassRule;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -14,9 +12,6 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 public class CreditCardRepositoryTest {
-
-	@ClassRule
-	public static PostgresqlDB postgresqlDB = PostgresqlDB.getInstance();
 
 	@Autowired
 	private CreditCardRepository creditCardRepository;
